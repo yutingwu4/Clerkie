07b801f24fe7eaad8079803fc10813e0b536c36d
Julie W <scorp.only@gmail.com>
2023-04-09 00:35:07 -0700

    card

---
+++ nextjs-blog/src/components/Card.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "@/styles/Home.module.css";
+
+function Card() {
+  return (
+    <div className={styles.card}>
+      George Bryant
+      <div>
+        <div className={styles.detail}>georgebryant@gmail.com</div>
+        <svg
+          width="4"
+          height="5"
+          viewBox="0 0 4 5"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle cx="2" cy="2.5" r="2" fill="#ABABAB" />
+        </svg>
+        <div className={styles.detail}>(393) 292-0983</div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- nextjs-blog/src/components/HomePage.js
+++ nextjs-blog/src/components/HomePage.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 import styles from "@/styles/Home.module.css";
-import FilterNav from '@/components/FilterNav';
+import Card from "@/components/Card";
 
 function HomePage() {
   return (
     <>
-      <div className={styles.home}>Welcome to the Clerkie Challenge!</div>
+      {/* <div className={styles.home}>Welcome to the Clerkie Challenge!</div> */}
+      <div className={styles.cardContainer}>
+        <Card />
+      </div>
     </>
   );
 }
