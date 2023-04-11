3b22a41b98f81e5dfbb67e2002c07ddec1755761
Julie W <scorp.only@gmail.com>
2023-04-11 15:01:28 -0700

    setting up mock api

--- nextjs-blog/src/pages/friends/index.js
+++ nextjs-blog/src/pages/friends/index.js
@@ -1,61 +1,26 @@
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import Page from "@/components/Page";
 import Card from "@/components/Card";
 import styles from "@/styles/Home.module.css";
 import FilterNav from "@/components/FilterNav";
-
-const data = [
-  {
-    name: "Sally Cooper",
-    phone: "(470) 782-5471",
-    email: "sallycooper@gmail.com",
-    status: "Close Friends",
-  },
-  {
-    name: "Judith Gonzalez",
-    phone: "(123) 142-4123",
-    email: "jgonzalez@gmail.com",
-    status: "",
-  },
-  {
-    name: "George Bryant",
-    phone: "(393) 292-0983",
-    email: "georgebryant@gmail.com",
-    status: "",
-  },
-  {
-    name: "Betty Wood",
-    phone: "(273) 098-1029",
-    email: "betty@gmail.com",
-    status: "Super Close Friends",
-  },
-  {
-    name: "Debra Russell",
-    phone: "(412) 235-5321",
-    email: "debrarussell@gmail.com",
-    status: "Super Close Friends",
-  },
-  {
-    name: "Rebecca Flores",
-    phone: "(574) 243-2574",
-    email: "rebeccaflores@gmail.com",
-    status: "Close Friends",
-  },
-  {
-    name: "Kim Holland",
-    phone: "(357) 345-6324",
-    email: "kimholland@gmail.com",
-    status: "",
-  },
-];
+import { getAllPeople } from '@/api/people';
 
 export default function Home() {
   const [selectedStatuses, setSelectedStatuses] = useState({});
+  const [data, setData] = useState([]);
 
   const filtersEnabled = useMemo(() => {
     console.log('filtersEnabled', Object.values(selectedStatuses).filter(el => el).length)
     return Object.values(selectedStatuses).filter(el => el).length;
   }, [selectedStatuses]);
+
+  useEffect(() => {
+    const fetchAllPeople = async () => {
+        const fetchedData = await getAllPeople();
+        setData(fetchedData);
+    }
+    fetchAllPeople();
+  }, [])
 
   return (
     <Page title="Friends">
